8a10b64eba85638fd79c541fc98b92c67ffb78b0
Rodrigo <rodrigojaviergd@gmail.com>
2021-01-01 21:08:08 -0400

    ------>

---
+++ app/src/main/java/com/rodrigo/javier/eox/hackermen/routerpointer/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rodrigo.javier.eox.hackermen.routerpointer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
